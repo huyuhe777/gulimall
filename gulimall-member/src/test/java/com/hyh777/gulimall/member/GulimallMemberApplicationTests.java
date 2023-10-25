c378317d625b2717e39c2f6bc95173cac051bb6e
hyh <stellahu1024@gmail.com>
2023-10-25 22:59:47 +0800

    init 5 modules

---
+++ gulimall-member/src/test/java/com/hyh777/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyh777.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/test/java/com/hyh777/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyh777.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
